f7d66bd989694dcbeeee0c39a0f8ae3865b276b3
Pier Luca Anania <p.l.anania@gmail.com>
2024-10-14 21:09:05 +1100

    Q Learning

---
+++ Q Learning.py
@@ -0,0 +1,14 @@
+import gym
+
+env = gym.make('MountainCar-v0',render_mode = 'human')
+env.reset()
+
+done = False
+
+while not done:
+    action = 2
+    new_state, reward, done, truncated, info = env.step(action)
+    done = done or truncated
+    env.render()
+
+env.close()
