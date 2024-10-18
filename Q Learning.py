184d5b841ed83d1fa74177efbb96bdb75d75409f
Pier Luca Anania <p.l.anania@gmail.com>
2024-10-18 13:22:52 +1100

    Q Learning

--- Q Learning.py
+++ Q Learning.py
@@ -6,7 +6,7 @@ env.reset()
 done = False
 
 while not done:
-    action = 1
+    action = 2
     new_state, reward, done, truncated, info = env.step(action)
     done = done or truncated
     env.render()
